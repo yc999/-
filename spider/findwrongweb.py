b4b68db4142eb89aa0660b0fdd1da54904dae37b
yc999 <31004500+yc999@users.noreply.github.com>
2021-02-16 20:31:32 +0800

    日常更新

--- spider/findwrongweb.py
+++ spider/findwrongweb.py
@@ -1,7 +1,14 @@
 #-- coding: utf-8 --
 # 找到爬过的非目标网页
+import sys
+# print(sys.path)
 import os
+print('\n')
+# print(os.path.realpath('./MLcode/'))
+sys.path.append(os.path.realpath('./MLcode'))
+import mytool
 
+print(sys.path)
 filepath = ''
 # fs = os.listdir(filepath)   #读取url目录
 
@@ -12,7 +19,6 @@ for root, dirs, files in os.walk(filepath):
          # root 表示当前正在访问的文件夹路径
          # dirs 表示该文件夹下的子目录名list
          # files 表示该文件夹下的文件list
-
         # 遍历文件
         #  for f in files:
         #      print(os.path.join(root, f))
@@ -21,23 +27,36 @@ for root, dirs, files in os.walk(filepath):
             dirlist.append(os.path.join(root, d))
             print(os.path.join(root, d))
 
+count_words_dic = {}
 
-import json
-def read_webdata(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file_to_read:
-        return json.loads(file_to_read.read())     
+# 读取停用词
+stopwordslist = []
+stopwordslist_path = ''
+stopwordslist =mytool.read_stopwords(stopwordslist_path)
+
 # 读取文件
 for dirpath in dirlist:
     for f in files:
-        data = read_webdata(os.path.join(root, f))
+        data = mytool.read_webdata(os.path.join(root, f))
         print(os.path.join(root, f))
+        # 网页数据存入一个list
+        target_data = mytool.get_all_webdata(data)
+        #分词
+        tmp_words = mytool.seg_sentence(target_data, stopwordslist)
+        #统计词出现次数
+        for word in tmp_words:
+            if word in count_words_dic:
+                count_words_dic[word] = count_words_dic[word]+1
+            else:
+                count_words_dic[word] = 0
+
+print(count_words_dic)
 
         #处理文件
+        # 文本分词
 
 
 
-import jieba
 
-rule = re.compile(u"[^\u4E00-\u9FA5]")
-sentence = rule.sub('',sentence)
-sentence_seged = jieba.lcut(sentence.strip(),cut_all=bool_cut_all)
+
+# import jieba
