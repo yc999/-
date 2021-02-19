c3a5f5a8d0a3ac985650ce5c7c84a661b0e5e4ae
yc999 <31004500+yc999@users.noreply.github.com>
2021-02-19 13:16:29 +0800

    找出可疑的内容

--- spider/findwrongweb.py
+++ spider/findwrongweb.py
@@ -6,13 +6,14 @@ import os
 print('\n')
 # print(os.path.realpath('./MLcode/'))
 sys.path.append(os.path.realpath('./MLcode'))
+sys.path.append(os.path.realpath('../MLcode'))
 import mytool
 
 print(sys.path)
 filepath = '/home/jiangy2/webdata'
 # fs = os.listdir(filepath)   #读取url目录
 
-
+wrongwordslist = ['福利', '视频', '澳门', '奥门', '投注', '色情']
 #读取文件夹
 dirlist = []
 for root, dirs, files in os.walk(filepath):
@@ -25,7 +26,7 @@ for root, dirs, files in os.walk(filepath):
         # 遍历所有的文件夹
         for d in dirs:
             dirlist.append(os.path.join(root, d))
-            print(os.path.join(root, d))
+            # print(os.path.join(root, d))
 
 count_words_dic = {}
 
@@ -35,28 +36,27 @@ stopwordslist_path = '/home/jiangy2/dnswork/stopwords/cn_stopwords.txt'
 stopwordslist =mytool.read_stopwords(stopwordslist_path)
 
 # 读取文件
+firststep_list = []
 for dirpath in dirlist:
     for f in files:
         data = mytool.read_webdata(os.path.join(root, f))
-        print(os.path.join(root, f))
+        # print(os.path.join(root, f))
         # 网页数据存入一个list
         target_data = mytool.get_all_webdata(data)
         #分词
         tmp_words = mytool.seg_sentence(target_data, stopwordslist)
         #统计词出现次数
         for word in tmp_words:
-            if word in count_words_dic:
-                count_words_dic[word] = count_words_dic[word]+1
-            else:
-                count_words_dic[word] = 0
-
-print(count_words_dic)
-
-        #处理文件
-        # 文本分词
-
-
-
-
-
-# import jieba
+            if word in wrongwordslist:
+                firststep_list.append(f)
+                break
+            # if word in count_words_dic:
+            #     count_words_dic[word] = count_words_dic[word]+1
+            # else:
+            #     count_words_dic[word] = 0
+print(firststep_list)
+# print(count_words_dic)
+# savefilepath = "./firststep.txt"
+# f = open(savefilepath, "w", encoding="utf-8")
+# f.write(json.dumps(webinfo, ensure_ascii=False))
+# f.close()#关闭文件
