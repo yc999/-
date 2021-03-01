1cf64358efa6b6fbb2ba3b8da34f0c83dc7b4ec3
yc999 <31004500+yc999@users.noreply.github.com>
2021-03-01 10:27:54 +0800

    1

--- MLcode/LDAmodel.py
+++ MLcode/LDAmodel.py
@@ -103,52 +103,37 @@ print(webdata_list[0])
 
 
 #构建词频矩阵，训练LDA模型
-webdata = read_webdata("E:/webdata/旅游网站/sh.tuniu.com.txt")
-print(webdata['title'])
-titlewordslist = mytool.seg_sentence(webdata['title']) #需要词嵌入时用False 传统机器学习用True
-print(titlewordslist)
-titlewordslist = seg_sentence("第224期",True) #需要词嵌入时用False 传统机器学习用True
-print(titlewordslist)
+dictionary = corpora.Dictionary(webdata_list)
+# corpus[0]: [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1),...]
+# corpus是把每条新闻ID化后的结果，每个元素是新闻中的每个词语，在字典中的ID和频率
+corpus = [dictionary.doc2bow(text) for text in webdata_list]
 
-raw_documents = [
-    '0无偿居间介绍买卖毒品的行为应如何定性',
-    '1吸毒男动态持有大量毒品的行为该如何认定',
-    '2如何区分是非法种植毒品原植物罪还是非法制造毒品罪',
-    '3为毒贩贩卖毒品提供帮助构成贩卖毒品罪',
-    '4将自己吸食的毒品原价转让给朋友吸食的行为该如何认定',
-    '5为获报酬帮人购买毒品的行为该如何认定',
-    '6毒贩出狱后再次够买毒品途中被抓的行为认定',
-    '7虚夸毒品功效劝人吸食毒品的行为该如何认定',
-    '8妻子下落不明丈夫又与他人登记结婚是否为无效婚姻',
-    '9一方未签字办理的结婚登记是否有效',
-    '10夫妻双方1990年按农村习俗举办婚礼没有结婚证 一方可否起诉离婚',
-    '11结婚前对方父母出资购买的住房写我们二人的名字有效吗',
-    '12身份证被别人冒用无法登记结婚怎么办？',
-    '13同居后又与他人登记结婚是否构成重婚罪',
-    '14未办登记只举办结婚仪式可起诉离婚吗',
-    '15同居多年未办理结婚登记，是否可以向法院起诉要求离婚'
-]
-corpora_documents = []
-for item_text in raw_documents:
-    # print(item_text)
-    item_str = jieba.lcut(item_text)
-    # print(item_str)
-    corpora_documents.append(item_str)
+lda = models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=3)
+topic_list = lda.print_topics(3)
+print("3个主题的单词分布为：\n")
+for topic in topic_list:
+    print(topic)
 
-print(corpora_documents)
 
-dictionary = corpora.Dictionary(corpora_documents)
-corpus = [ dictionary.doc2bow(text) for text in corpora_documents ]
-print(corpus)
-# lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=10)
-# for words in titlewordslist:
-#     print(words)
 
-# for line in raw_documents:
-#         line = line.split('\t')[1]
-#         line = re.sub(r'[^\u4e00-\u9fa5]+','',line)
-#         line_seg = seg_depart(line.strip())
-#         outputs.write(line_seg.strip() + '\n')
+# raw_documents = [
+#     '0无偿居间介绍买卖毒品的行为应如何定性',
+#     '1吸毒男动态持有大量毒品的行为该如何认定',
+#     '2如何区分是非法种植毒品原植物罪还是非法制造毒品罪',
+#     '3为毒贩贩卖毒品提供帮助构成贩卖毒品罪',
+#     '4将自己吸食的毒品原价转让给朋友吸食的行为该如何认定',
+#     '5为获报酬帮人购买毒品的行为该如何认定',
+#     '6毒贩出狱后再次够买毒品途中被抓的行为认定',
+#     '7虚夸毒品功效劝人吸食毒品的行为该如何认定',
+#     '8妻子下落不明丈夫又与他人登记结婚是否为无效婚姻',
+#     '9一方未签字办理的结婚登记是否有效',
+#     '10夫妻双方1990年按农村习俗举办婚礼没有结婚证 一方可否起诉离婚',
+#     '11结婚前对方父母出资购买的住房写我们二人的名字有效吗',
+#     '12身份证被别人冒用无法登记结婚怎么办？',
+#     '13同居后又与他人登记结婚是否构成重婚罪',
+#     '14未办登记只举办结婚仪式可起诉离婚吗',
+#     '15同居多年未办理结婚登记，是否可以向法院起诉要求离婚'
+# ]
 
 
 
