633e061dd6006bf498feb5ed82a1e6f27b4d29be
yc999 <31004500+yc999@users.noreply.github.com>
2021-03-04 19:27:04 +0800

    12

--- trainclass/test.py
+++ trainclass/test.py
@@ -10,8 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import UnexpectedAlertPresentException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+
 s=requests.session()
 s.keep_alive = False
 url =  "ebrun.com"
