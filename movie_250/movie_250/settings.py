e46bca75652fd85c1b168e0058b2c47698218550
yc999 <31004500+yc999@users.noreply.github.com>
2021-04-02 12:58:49 +0800

    1

--- movie_250/movie_250/settings.py
+++ movie_250/movie_250/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for myfirstPj project
+# Scrapy settings for movie_250 project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,25 +7,25 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'myfirstPj'
+BOT_NAME = 'movie_250'
 
-SPIDER_MODULES = ['myfirstPj.spiders']
-NEWSPIDER_MODULE = 'myfirstPj.spiders'
+SPIDER_MODULES = ['movie_250.spiders']
+NEWSPIDER_MODULE = 'movie_250.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0'
+#USER_AGENT = 'movie_250 (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -45,13 +45,13 @@ DOWNLOAD_DELAY = 3
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'myfirstPj.middlewares.MyfirstpjSpiderMiddleware': 543,
+#    'movie_250.middlewares.Movie250SpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'myfirstPj.middlewares.MyfirstpjDownloaderMiddleware': 543,
+#    'movie_250.middlewares.Movie250DownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ DOWNLOAD_DELAY = 3
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'myfirstPj.pipelines.MyfirstpjPipeline': 300,
+#    'movie_250.pipelines.Movie250Pipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
