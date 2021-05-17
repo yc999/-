2446afe3e4aeaa3fece20c608e55f286e7daa3ab
yc999 <31004500+yc999@users.noreply.github.com>
2021-05-17 13:40:50 +0800

    1

--- MLcode/classcount.py
+++ MLcode/classcount.py
@@ -9,6 +9,7 @@ from keras.models import Sequential
 # from keras.layers import Dropout,Dense,Embedding,LSTM,Activation
 from keras.layers import Dense, LSTM, Embedding, Dropout, Conv1D, MaxPooling1D, Bidirectional, Activation,Masking
 import pickle
+from numpy.lib.shape_base import expand_dims
 from sklearn.model_selection import train_test_split
 from gensim.corpora.dictionary import Dictionary
 import re
@@ -81,6 +82,36 @@ del tc_wv_model
 
 
 
+#  85 
+# 107 110 112 114  125
+'电子元器件': 85,'球类运动': 107,'法律法规': 110,'母婴网站': 112,'网址导航': 114,
+'娱乐时尚': 125, 
+
+class_index = {'商务服务': 0, '教育资讯': 1, '动漫网站': 2, '返利比价': 3, '票务预订': 4, '出国留学': 5,
+   '家电数码': 6, '音乐网站': 7, '健康资讯': 8, '门户网站': 9, '生活百科': 10, '虚拟现实': 11, 
+   '旅行社': 12, '搜索引擎': 13, '视频电影': 14, '医院诊所': 15, '信息公司': 16, '广播电视': 17,
+   '医学知识': 18, '房产网站': 19, '汽车配件': 20, ' 外语综合': 21, '在线教育': 22, '图书展馆': 23, 
+   '新闻报刊': 24, '减肥瘦身': 25, '建筑材料': 26, '分类信息': 27, '招商加盟': 28, '电力水务': 29,
+   '家居建材': 30, '网络硬盘': 31, '博客网站': 32, '健康保健': 33, '药品交易': 34, '日化用品': 35, 
+   '培训机构': 36, 'QQ个性': 37, '棋牌健身': 38, '农林畜牧渔': 39, '极限运动': 40, 
+   '域名主机': 41, '医疗器械': 42, '社科文艺': 43, '生育避孕': 44, '网络安全': 45,
+    '汽车网站': 46, '聊天交友': 47, '地方网站': 48, '体育院校': 49, '家政服务': 50, 
+    '金融财经': 51, '事业单位': 52, '军事国防': 53, 
+   '体育用品': 54, '福彩体彩': 55, '酒店宾馆': 56, '常用查询': 57, '政府门户': 58, '团购网站': 59, 
+   '设计素材': 60, '交易平台': 61, '电商服务': 62, '美容整形': 63, '收藏爱好': 64, '星座运势': 65, 
+   '技术编程': 66, '广电通信': 67, '物流运输': 68, '旅游网站': 69, '男科医院': 70, '商业百货': 71, 
+   '宠物玩具': 72, '驾校学车': 73, '广告营销': 74, '中小学校': 75, '邮件通信': 76, '银行保险': 77, 
+   '创业投资': 78, '不孕不育': 79, '应用工具': 80, '购物分享': 81, '网贷平台': 82, '求职招聘': 83, 
+   '社交网站': 84, '电子元器件': 85, '医疗职业': 86, '企业信息': 87, '纺织皮革': 88, '食品饮料': 89, 
+   '汽车厂商': 90, '高等院校': 91, '电子商务': 92, '心理健康': 93, '政府职能': 94, '机械工业': 95, 
+   '电脑硬件': 96, '影楼婚嫁': 97, '组织机构': 98, '家居小商品': 99, '幽默笑话': 100, '广告联盟': 101,
+   '站长资源': 102, '化工能源': 103, '武术搏击': 104, '五金电工': 105, '论坛综合': 106, '球类运动': 107,
+   '卫星地图': 108, '图片摄影': 109, '法律法规': 110, '水暖安防': 111, '母婴网站': 112, '服装配饰': 113,
+   '网址导航': 114, '数据分析': 115, '教育考试': 116, '科学探索': 117, '单车汽摩': 118, '药品药学': 119, 
+   '天文历史': 120, '包装印刷': 121, '医疗药企': 122, '游戏网站': 123, '明星粉丝': 124, '娱乐时尚': 125, 
+   '证券网站': 126, '体育综合': 127, '妇幼医院': 128, '手机数码': 129, 'IT资讯': 130, '电子支付': 131, 
+   '软件下载': 132, '石化能源': 133, '百科 辞典': 134, '餐饮美食': 135, '公共团体': 136, '交通地图': 137, 
+   '违法网站': 138, '小说网站': 139, '电商网站': 140}
 
 #步骤2 数据预处理
 
@@ -90,6 +121,7 @@ del tc_wv_model
 stopwordslist = []  # 停用词列表
 stopwords_path = "/home/jiangy2/dnswork/stopwords/cn_stopwords.txt"
 stopwordslist = mytool.read_stopwords(stopwords_path)
+
 
 
 #2.2 设置分类类别
@@ -108,20 +140,16 @@ for i,filename in enumerate(fs):
             parts = line.split(",")
             if  not line:
                 break
-            weburllist.append(parts[1])
-            classlist.append(parts[0])
+            if len(parts)>1:
+                weburllist.append(parts[1])
+                classlist.append(parts[0])
     tmp = filename.split(".")[0]
     class_index[tmp] = i
 
-print(class_index)
+print(weburllist)
 
 # classtype 保存了所有的分类信息  子类名 ： 父类目
 # class_index 保存了父类名对应的下标
-
-
-
-
-
 
 
 
@@ -133,25 +161,52 @@ Y_train = []
 
 #读取保存的网页信息
 # path = "D:/dnswork/sharevm/topchinaz/"
-path = "/home/jiangy2/webdata/"
-fs = os.listdir(path)
+
+
+
 i=0
 j=0
-for subpath in fs:
-    filepath = os.path.join(path, subpath)
-    # print(filepath)
-    if (os.path.isdir(filepath)):
-        webdata_classtype = classtype[subpath]  # 查询父类别
-        webdata_class_index = class_index[webdata_classtype] #父类别下标
-        webdata_path = os.listdir(filepath)
-        for filename in webdata_path:
-            i=i+1
-            webdata = mytool.read_webdata(os.path.join(filepath, filename))
-            if webdata['title'] != "" and webdata['description'] != "" and webdata['keywords'] != "":
-                if len(webdata['webtext'])>=15:
-                    j=j+1
-                    X_train_text.append(mytool.get_all_webdata(webdata))
-                    Y_train.append(webdata_class_index)
+def find_weburldata(weburl):
+    path = "/home/jiangy2/webdata/"
+    fs = os.listdir(path)
+    for subpath in fs:
+        filepath = os.path.join(path, subpath)
+        if (os.path.isdir(filepath)):
+            webdata_path = os.listdir(filepath)
+            for filename in webdata_path:
+                fileurl = filename.replace(".txt","")
+                fileurl = fileurl.replace("www.","")
+                stripfilename = weburl.replace("www.","")
+                if fileurl == stripfilename:
+                    webdata = mytool.read_webdata(os.path.join(filepath, filename))
+                    if webdata['title'] != "" and webdata['description'] != "" and webdata['keywords'] != "":
+                        if len(webdata['webtext'])>=15:
+                            return mytool.get_all_webdata(webdata)          
+    newpath = "/home/jiangy2/dnswork/newwebdata/"
+    newfs = os.listdir(newpath)
+    for subpath in newfs:
+        filepath = os.path.join(newpath, subpath)
+        if (os.path.isdir(filepath)):
+            webdata_path = os.listdir(filepath)
+            for filename in webdata_path:
+                fileurl = filename.replace(".txt","")
+                fileurl = fileurl.replace("www.","")
+                stripfilename = weburl.replace("www.","")
+                if fileurl == stripfilename:
+                    webdata = mytool.read_webdata(os.path.join(filepath, filename))
+                    if webdata['title'] != "" and webdata['description'] != "" and webdata['keywords'] != "":
+                        if len(webdata['webtext'])>=15:
+                            return mytool.get_all_webdata(webdata)
+    return ""
+
+
+for index,weburl in enumerate(weburllist):
+    i = i+1
+    webdata = find_weburldata(weburl)
+    if webdata != "":
+        j = j + 1
+        X_train_text.append(webdata)
+        Y_train.append(class_index[classlist[index]])
 
 
 print("已爬取网页数：")
@@ -159,7 +214,7 @@ print(i)
 print("有效网页数：")
 print(j)
 
-
+ids = list(set(Y_train))
 
 
 
@@ -226,10 +281,11 @@ def get_lstm_model():
 # x_train, x_test, y_train, y_test = train_test_split(X_padded, Y, test_size=0.2)
 
 Y=to_categorical(Y_train, len(class_index))
-x_train, x_test, y_train, y_test = train_test_split(X_train, Y, test_size=0.1)
+# x_train, x_test, y_train, y_test = train_test_split(X_train, Y, test_size=0.1)
 
-x_train_raw = pad_sequences(x_train, maxlen=model_max_len)
-x_test_raw = pad_sequences(x_test, maxlen=model_max_len)
+
+x_train_raw = pad_sequences(X_train, maxlen=model_max_len)
+# x_test_raw = pad_sequences(x_test, maxlen=model_max_len)
 
 
 
@@ -240,13 +296,13 @@ def model_fit(model, x, y):
     return model.fit(x, y, batch_size=10, epochs=5, validation_split=0.1)
 
 model = get_lstm_model()
-model_train = model_fit(model, x_train_raw, y_train)
+model_train = model_fit(model, x_train_raw, Y)
 
 
 # 3.4 测试
-print(model.evaluate(x_test_raw, y_test))
+# print(model.evaluate(x_test_raw, y_test))
 
-modelsave_path = "/home/jiangy2/dnswork/modeldir/LSTMmodel"
+modelsave_path = "/home/jiangy2/dnswork/modeldir/tmp_LSTMmodel"
 model.save(modelsave_path)
 
 
